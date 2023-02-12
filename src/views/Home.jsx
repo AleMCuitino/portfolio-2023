acc2191a9dfd52063e74d2ad8bcb2ab86625b0c1
AleMCuitino <ale.morales11@gmail.com>
2023-02-12 15:59:44 +0100

    Created view home, with navbar, texts and imgs.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Boarding from '../component/boarding/Boarding';
+import Navbar from "../component/navbar/Navbar";
+// import Footer from "../../components/footer/Footer";
+// import ShowElement from '../../views/crud/ShowElement';
+// import Boarding from "../../components/boarding/Boarding";
+// import Box from '@mui/material/Box';
 
-const Home = () => {
-  return (
-    <div>
-      Mi portafolio
-    </div>
-  )
+export default function Home() {
+
+    return (
+        <React.Fragment>
+            <Navbar />
+            <Boarding />
+        </React.Fragment>
+    );
 }
-
-export default Home
