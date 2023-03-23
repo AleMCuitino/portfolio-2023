53b1b45f675a9b2d36bc3ed27684f3adb290203e
AleMCuitino <ale.morales11@gmail.com>
2023-03-23 17:26:21 +0100

    Edited styles boarding home

--- src/component/boarding/Boarding.jsx
+++ src/component/boarding/Boarding.jsx
@@ -6,88 +6,61 @@ import ImgBoarding from '../img-boarding/ImgBoarding';
 
 const Boarding = () => {
     return (
-        <React.Fragment>
-            <Grid
-                container
-                spacing={0}
-                direction="column"
-                flexDirection="wrap"
-                alignItems="center"
-                justifyContent="center"
-                style={{
-                    maxHeight: '100vh',
-                }}>
-                {/* <div style={{ width: '50%', mt: { xs: 2, sm: 2.5, md: 3, lg: 3.5, xl: 4 }, display: 'flex', flexDirection: 'row-reverse', justifyContent: 'end' }}>
-                    <Typography
-                        variant="p"
-                        sx={{
-                            mb: 2,
-                            display: { xs: 'flex', md: 'flex' },
-                            fontWeight: 200,
-                            fontStyle: 'italic',
-                            textTransform: 'uppercase',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        Diseño y Desarrollo Web Front-end
-                    </Typography>
-                </div> */}
-                <Box
+        <Box sx={{
+            width: '100%',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+        }}>
+            <Box sx={{
+                mr: 10,
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+                <Typography
+                    variant="h1"
                     sx={{
-                        width: '50%',
-                        display: 'flex',
+                        display: { xs: 'flex', md: 'flex' },
+                        justifyItems: 'center',
                         alignItems: 'center',
-                        justifyContent: 'start'
-                    }}>
-                    <Typography
-                        variant="h1"
-                        sx={{
-                            display: { xs: 'flex', md: 'flex' },
-                            justifyItems: 'center',
-                            alignItems: 'center',
-                            fontSize: 150,
-                            fontWeight: 700,
-                            textDecoration: 'none',
-                            color: "#E9417B"
-                        }}
-                    >
-                        portfolio
-                    </Typography>
-                </Box>
-                <Box sx={{
-                    width: '50%',
-                    // ml: { xs: 2, sm: 2, md: 2 },
-                    display: 'flex',
-                    // flexDirection: { xs: 'column-reverse', sm: 'column-reverse', md: 'row', lg: 'row', xl: 'row' },
-                    alignItems: 'center',
-                    justifyContent: 'start',
-                    // alignItems: 'flex-end'
-                }}>
-
-                    <Typography
-                        variant="p"
-                        sx={{
-                            mt: { xs: 2, sm: 3, md: 5, lg: 5, xl: 10 },
-                            // mr: { xs: 0, sm: 0, md: 2, lg: 2, xl: 3 },
-                            display: { xs: 'flex', md: 'flex' },
-                            height: 'auto',
-                            color: "#E9417B",
-                            width: 250,
-                            fontSize: 16,
-                            fontWeight: { xs: 10, sm: 200 },
-                            // textAlign: 'right',
-                            textDecoration: 'none',
-                            justifyItems: 'center',
-                            alignItems: { xs: 'flex-start', sm: 'flex-start', md: 'center' }
-                        }}
-                    >
-                        ¡Hola! Soy Alejandra, desarrolladora web junior frontend y diseñadora integral.
-                        Por ahora, este portfolio está en proceso constructivo.
-                    </Typography>
-                    {/* <ImgBoarding /> */}
-                </Box>
-            </Grid>
-        </React.Fragment >
+                        fontSize: 150,
+                        fontWeight: 700,
+                        textDecoration: 'none',
+                        color: "#E9417B",
+                        '&::after': {
+                            content: '"Web"',
+                            fontSize: 80,
+                            color: " #F5DC4C",
+                            position: 'absolute',
+                            right: 50,
+                            top: 350,
+                        }
+                    }}
+                >
+                    portfolio
+                </Typography>
+                <Typography
+                    variant="p"
+                    sx={{
+                        mt: { xs: 2, sm: 2, md: 3, lg: 3, xl: 10 },
+                        // mr: { xs: 0, sm: 0, md: 2, lg: 2, xl: 3 },
+                        display: { xs: 'flex', md: 'flex' },
+                        height: 'auto',
+                        color: "#E9417B",
+                        width: 250,
+                        fontSize: 18,
+                        fontWeight: { xs: 10, sm: 300 },
+                        textDecoration: 'none',
+                        justifyItems: 'center',
+                        alignItems: { xs: 'flex-start', sm: 'flex-start', md: 'center' },
+                    }}
+                >
+                    ¡Hola! Soy Alejandra, desarrolladora web frontend junior y diseñadora integral.
+                </Typography>
+                {/* <ImgBoarding /> */}
+            </Box>
+        </Box >
     )
 }
 
