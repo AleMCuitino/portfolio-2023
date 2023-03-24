085deef43dbb88dce47a69ddcbc682faf325ec23
AleMCuitino <ale.morales11@gmail.com>
2023-03-24 13:52:32 +0100

    Added slider homew

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import Boarding from '../component/boarding/Boarding';
+import Slider from '../component/icons/Slider';
 import Navbar from "../component/navbar/Navbar";
-// import Footer from "../../components/footer/Footer";
-// import ShowElement from '../../views/crud/ShowElement';
-// import Boarding from "../../components/boarding/Boarding";
-// import Box from '@mui/material/Box';
 
 export default function Home() {
 
@@ -12,6 +9,7 @@ export default function Home() {
         <React.Fragment>
             <Navbar />
             <Boarding />
+            <Slider />
         </React.Fragment>
     );
 }
