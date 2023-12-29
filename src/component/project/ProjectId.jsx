5d09615431614065e925557681f4db19bf14bb23
AleMCuitino <ale.morales11@gmail.com>
2023-12-30 00:16:56 +0100

    Add data to project FitApp

--- src/component/project/ProjectId.jsx
+++ src/component/project/ProjectId.jsx
@@ -10,30 +10,8 @@ import { Box } from '@mui/system';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 
-const useParallaxBanner =
-    setScrollPosition => {
-        const handleScroll = () =>
-            setScrollPosition(window.pageYOffset);
-
-        useEffect(() => {
-            window
-                .addEventListener(
-                    "scroll",
-                    handleScroll,
-                    { passive: true }
-                );
-
-            return () =>
-                window.removeEventListener(
-                    "scroll", handleScroll);
-        }, []);
-    }
 
 const ProjectId = () => {
-    const [scrollPosition, setScrollPosition] =
-        useState(0)
-
-    useParallaxBanner(setScrollPosition);
 
     const [project, setProject] = useState({});
     const { id } = useParams()
@@ -62,20 +40,18 @@ const ProjectId = () => {
                     className="banner"
                     image={project?.imgBanner}
                     style={{
-                        backgroundImage: `url(${project?.imgBanner})`,
-                        backgroundSize:
-                            `${(window.outerHeight - scrollPosition) / 3
-                            }%`,
+                        backgroundImage: `url(${project?.imgBanner})`
                     }}
                 >
                 </section>
                 <Box
                     sx={{
                         mx: { xs: 5, md: 10, lg: 20, xl: 25 },
-                        mt: {xs: 5, sm: 10, md: 20 },
+                        mt: { xs: 5, sm: 10, md: 20 },
+                        mb: { xs: 5, sm: 10, md: 20 },
                         display: 'flex',
                         flexDirection: { xs: 'column', md: 'row' },
-                        justifyContent: {xs: 'flex-start', md: 'space-between' },
+                        justifyContent: { xs: 'flex-start', md: 'space-between' },
                         gap: { xs: '2rem', sm: '4rem', md: '8rem', lg: '10rem' }
                     }}
                 >
@@ -83,33 +59,33 @@ const ProjectId = () => {
                         <Typography
                             variant='h3'
                             sx={{
-                                mb: {xs: 2, sm: 3, md: 4 },
+                                mb: { xs: 2, sm: 3, md: 4 },
                                 fontWeight: '600',
                             }}
                         >
                             {project?.title}
                         </Typography>
-                        <Box
+                        {/* <Box
                             mb={5}
                             sx={{
                                 display: 'flex',
                                 justifyContent: 'flex-start',
                             }}>
                             <Link to={project?.linkRepo} target="_blank">
-                                <IconButton size="large" sx={{ "&:hover": {color: "#E9417B"}}}>
+                                <IconButton size="large" sx={{ "&:hover": { color: "#E9417B" } }}>
                                     <GitHubIcon fontSize="medium" />
                                 </IconButton>
                             </Link>
                             <Link to={project?.linkDemo} target="_blank">
-                                <IconButton size="large" sx={{ "&:hover": {color: "#E9417B"}}}>
+                                <IconButton size="large" sx={{ "&:hover": { color: "#E9417B" } }}>
                                     <RocketLaunchIcon fontSize="medium" />
                                 </IconButton>
                             </Link>
-                        </Box>
+                        </Box> */}
                         <Typography
                             variant='p'
                             sx={{
-                                mb: {xs: 2, sm: 3, md: 4 },
+                                mb: { xs: 2, sm: 3, md: 4 },
                                 fontWeight: '300',
                                 fontSize: { xs: '1em', md: '1.25em', xl: '1.5em' },
                                 textAlign: 'justify'
@@ -121,7 +97,7 @@ const ProjectId = () => {
                     <CardMedia
                         component="img"
                         image={project?.imgUrl}
-                        alt="green iguana"
+                        alt="imagen de marca del proyecto"
                         sx={{
                             maxHeight: { xs: 350, sm: 450, xl: 650 },
                             maxWidth: { xs: 350, sm: 450, xl: 650 }
@@ -130,54 +106,45 @@ const ProjectId = () => {
                 </Box>
                 <Box
                     sx={{
-                        mx: { xs: 5, md: 10, lg: 20, xl: 25 },
-                        my: {xs: 5, sm: 10, md: 20 },
+                        // mx: { xs: 5, md: 10, lg: 20, xl: 25 },
+                        // my: { xs: 5, sm: 10, md: 20 },
                         display: 'flex',
-                        flexDirection: { xs: 'column', sm: 'column', md: 'row'},
+                        flexDirection: { xs: 'column', sm: 'column', md: 'row' },
                         justifyContent: 'center',
                         alignItems: 'flex-start',
-                        gap: { md: '5rem', lg: '8rem' }
                     }}>
-                    
+
                     {Array.isArray(project.tech) && (
-                    <Box
-                        sx={{
-                            width: '50%',
-                            display: 'flex',
-                            flexWrap: 'wrap',
-                            justifyContent: 'space-between',
-                            alignItems: 'flex-start',
-                            gap: { xs: '2rem', sm: '3rem', md: '5rem' },
+                        <Box
+                            sx={{
+                                width: '50%',
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                justifyContent: 'space-between',
+                                alignItems: 'flex-start',
+                                // gap: { xs: '2rem', sm: '3rem', md: '5rem' },
                             }}>
-                                {project.tech.map((tech, index) => (
-                                <img 
-                                    src={tech} 
-                                    alt={index} 
+                            {project.tech.map((tech, index) => (
+                                <img
+                                    src={tech}
+                                    alt={index}
                                     sx={{
-                                        maxWidth: { xs: '20%', sm: '20%', md: '80%', lg: '100%'},
+                                        maxWidth: { xs: '20%', sm: '20%', md: '80%', lg: '100%' },
                                         height: 'auto'
-                                        }} ></img>
-                                ))}
-                            </Box>
-                            )}
-                    <Typography
-                        variant='p'
-                        sx={{
-                            width: '50%',
-                            fontWeight: '300',
-                            fontSize: { xs: '1em', md: '1.25em', xl: '1.5em' },
-                            textAlign: 'justify'
-                        }}
-                        >
-                            {project?.description2}
-                    </Typography>
+                                    }} ></img>
+                            ))}
+                        </Box>
+                    )}
+                    
+                    
                 </Box>
                 <Box
                     sx={{
-                        mx: {xs: 5, sm: 10, md: 20 },
+                        mx: { xs: 5, sm: 10, md: 15 },
                         display: 'flex',
                         justifyContent: 'space-between',
                         alignItems: 'flex-end',
+                        gap: { md: '1rem', lg: '2rem' }
                     }}>
                     {Array.isArray(project.imgProject) && (
                         <Box
@@ -186,13 +153,52 @@ const ProjectId = () => {
                             flexWrap: 'wrap',
                             justifyContent: 'space-between',
                             alignItems: 'flex-start',
-                            gap: { xs: '2rem', sm: '3rem', md: '5rem' },
+                            // gap: { xs: '2rem', sm: '3rem', md: '1rem' },
                         }}>
                             {project.imgProject.map((imgProject, index) => (
                             <img src={imgProject} alt={index}></img>
                             ))}
                         </Box>
                     )}
+                    <section>
+                        <Typography
+                            variant='h4'
+                            sx={{
+                                mb: { xs: 3, sm: 5, md: 5 },
+                                fontWeight: '400',
+                            }}
+                        >
+                            {project?.titlegoal}
+                        </Typography>
+                        {/* <Box
+                            mb={5}
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'flex-start',
+                            }}>
+                            <Link to={project?.linkRepo} target="_blank">
+                                <IconButton size="large" sx={{ "&:hover": { color: "#E9417B" } }}>
+                                    <GitHubIcon fontSize="medium" />
+                                </IconButton>
+                            </Link>
+                            <Link to={project?.linkDemo} target="_blank">
+                                <IconButton size="large" sx={{ "&:hover": { color: "#E9417B" } }}>
+                                    <RocketLaunchIcon fontSize="medium" />
+                                </IconButton>
+                            </Link>
+                        </Box> */}
+                        <Typography
+                            variant='p'
+                            sx={{
+                                mb: { xs: 2, sm: 3, md: 4 },
+                                fontWeight: '300',
+                                fontSize: { xs: '1em', md: '1.25em', xl: '1.5em' },
+                                textAlign: 'justify'
+                            }}
+                        >
+                            {project?.goals}
+                        </Typography>
+                    </section>
                 </Box>
             </Grid>
         </React.Fragment>
